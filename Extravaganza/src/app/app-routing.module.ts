9c9d59285a117276dd34823af709ebb763037873
Greg Vondra <gvondra@gmail.com>
2018-08-18 11:13:14 -0500

    add disclaimer

--- Extravaganza/src/app/app-routing.module.ts
+++ Extravaganza/src/app/app-routing.module.ts
@@ -1,11 +1,20 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from './home/home.component';
-
+import { SponsorComponent } from './sponsor/sponsor.component';
+import { DisclaimerComponent } from './disclaimer/disclaimer.component';
 const routes: Routes = [
   {
     path: "",
     component: HomeComponent
+  },
+  {
+    path: "sponsor",
+    component: SponsorComponent
+  },
+  {
+    path: "disclaimer",
+    component: DisclaimerComponent
   }
 ];
 
--- Extravaganza/src/app/app.module.ts
+++ Extravaganza/src/app/app.module.ts
@@ -6,13 +6,17 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { MenuComponent } from './menu/menu.component';
+import { SponsorComponent } from './sponsor/sponsor.component';
+import { DisclaimerComponent } from './disclaimer/disclaimer.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
-    MenuComponent
+    MenuComponent,
+    SponsorComponent,
+    DisclaimerComponent
   ],
   imports: [
     BrowserModule,
