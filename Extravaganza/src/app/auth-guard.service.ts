eaaa9b0b0623c56494ad7aa7553efebc81a20340
Greg Vondra <gvondra@gmail.com>
2018-09-07 12:22:25 -0500

    assert roles

--- Extravaganza/src/app/auth-guard.service.ts
+++ Extravaganza/src/app/auth-guard.service.ts
@@ -7,12 +7,28 @@ export class AuthGuardService implements CanActivate {
 
   constructor(public auth: AuthService, public router: Router) {}
 
-  canActivate(): boolean {
-    if (!this.auth.isAuthenticated()) {
-      this.router.navigate(['']);
-      return false;
+  canActivate(): Promise<boolean> | boolean {
+    if (this.auth.isAuthenticated()) { 
+      if (this.auth.userProfile) {
+        let profile = this.auth.userProfile;
+        return (profile && (profile['http://vondra/thanksgiving-write'] == 1 || profile['http://vondra/thanksgiving-read'] == 1))
+      } else {
+        return new Promise(          
+          (resolve, reject) => {
+            this.auth.getProfile(
+              (err, profile) => {
+                resolve(profile && (profile['http://vondra/thanksgiving-write'] == 1 || profile['http://vondra/thanksgiving-read'] == 1));
+              }
+            )
+          }).then(r => {
+            if (!r){
+              this.router.navigate(['']);          
+            }
+            return r as boolean
+          });
+      }    
     }
-    return true;
-}
-
+    this.router.navigate(['']);
+    return false;
+  }
 }
