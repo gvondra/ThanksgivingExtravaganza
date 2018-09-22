408ea7d69643049d56771285d16d89cbf004f440
Greg Vondra <gvondra@gmail.com>
2018-09-22 11:38:38 -0500

    update callback url

--- Extravaganza/src/environments/environment.prod.ts
+++ Extravaganza/src/environments/environment.prod.ts
@@ -2,5 +2,5 @@ export const environment = {
   production: true,
   clientId: "q3Afa42bxxeZAq1w1UtFu9sxslBKIURR",
   authDomain: "vondra.auth0.com",
-  callbackUrl: "http://thanksgivingextravaganza2.centralus.azurecontainer.io//callback"
+  callbackUrl: "http://thanksgivingextravaganza3.centralus.azurecontainer.io//callback"
 };
